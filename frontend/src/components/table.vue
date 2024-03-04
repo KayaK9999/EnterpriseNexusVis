2ee901d1c8f3d4604317858551533c9a1aa174fd
hechong <1522442815@qq.com>
2024-03-04 20:34:58 +0800

    feat: radar, table

---
+++ frontend/src/components/table.vue
@@ -0,0 +1,21 @@
+<template>
+    <el-table 
+    :data="fields" max height="1000" border style="width: 100%">
+      <el-table-column type="index" width="50" />
+      <el-table-column  prop="date" label="Date" width="180" />
+      <el-table-column prop="name" label="Name" width="180" />
+      <el-table-column  prop="address" label="Address" width="300"/>
+    </el-table>
+  </template>
+  
+  <script>
+
+export default {
+  name: 'Table',
+  props:[
+    'fields'
+  ]
+
+}
+  </script>
+  
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-const app = createApp(App)
-app.mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+createApp(App).use(ElementPlus).mount('#app')
 
